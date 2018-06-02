6040490d0f0827e1f91c58372afd1b223e3d6e33
Adam Urbina <adamurbina@gmail.com>
2018-06-02 18:37:42 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,14 +1,14 @@
 class Dog
     def initialize(dog_name)
-        @dog_name = dog_name
+        @my_name = dog_name
     end
 
     def name
-        @dog_name
+        @my_name
     end
 end
 
 Doggo = Dog.new("Yeezy")
 puts Doggo.name
-Doggo.instance_variable_set(:@dog_name, "Alaxander")
+Doggo.instance_variable_set(:@my_name, "Alaxander")
 puts Doggo.name
